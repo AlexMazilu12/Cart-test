29a60285206e230772dec045af17ccfd06b8c5ac
Alexandru Mazilu <alexst.mazilu@gmail.com>
2023-11-04 15:48:54 +0100

    I changed dad

--- cart copy.py
+++ cart copy.py
@@ -68,6 +68,7 @@ def process_order():
             'order_id': order_id,
             'name': name,
             'cart': session['cart']
+            dad
         }
         orders[order_id] = order
         session['luigi']=session['cart']
